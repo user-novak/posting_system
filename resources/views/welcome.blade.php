a211f2442e0fe236711dc2a79c3144c3af9985b8
novak <novakmzv@gmail.com>
2021-09-12 00:18:55 -0500

    creacion de home 2.0

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,8 +1,10 @@
 <x-app-layout>
-    <section class="bg-cover" style="background-image: url({{ asset('img/home/digital.jpg') }})">
+    
+    {{-- commercial section --}}
+    <section class="bg-cover" style="background-image: url({{ asset('img/home/sale.jpg') }})">
         <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-36">
             <div class="w-full md:w-3/4 lg:w-1/2">
-                <h1 class="text-white font-bold text-4xl">Domina la tegnologia</h1>
+                <h1 class="text-white font-bold text-4xl">Encuentra eso que estas buscando</h1>
                 <p class="text-white text-lg mt-2 mb-4">
                     Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi explicabo blanditiis adipisci fugiat
                     dolorum vero fugit enim sapiente illo asperiores, nam ad at quia, aperiam quis doloremque magni
@@ -21,17 +23,18 @@
         </div>
     </section>
 
+    {{-- content section --}}
     <section class="mt-24">
-        <h1 class="text-grey-600 text-center text-3xl mb-6">CONTENIDO</h1>
+        <h1 class="text-grey-600 text-center text-3xl mb-6">TE OFRECEMOS</h1>
         <div
-            class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-x-6 gap-y-8">
+            class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-x-8 gap-y-8">
             <article>
                 <figure>
-                    <img class="rounded-xl h-36 w-full object-cover" src="{{ asset('img/home/digital.jpg') }}" alt="">
+                    <img class="rounded-xl h-36 w-full object-cover" src="{{ asset('img/home/producto.png') }}" alt="">
                 </figure>
 
                 <header class="mt-2">
-                    <h1 class="text-center text-xl text-gray-700">gato</h1>
+                    <h1 class="text-center text-xl text-gray-700">Productos</h1>
                 </header>
 
                 <p class="text-sm text-gray-500">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quia aliquid
@@ -40,11 +43,11 @@
 
             <article>
                 <figure>
-                    <img class="rounded-xl h-36 w-full object-cover" src="{{ asset('img/home/digital.jpg') }}" alt="">
+                    <img class="rounded-xl h-36 w-full object-cover" src="{{ asset('img/home/servicio.png') }}" alt="">
                 </figure>
 
                 <header class="mt-2">
-                    <h1 class="text-center text-xl text-gray-700">girl</h1>
+                    <h1 class="text-center text-xl text-gray-700">Servicios</h1>
                 </header>
 
                 <p class="text-sm text-gray-500">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quia aliquid
@@ -53,11 +56,11 @@
 
             <article>
                 <figure>
-                    <img class="rounded-xl h-36 w-full object-cover" src="{{ asset('img/home/digital.jpg') }}" alt="">
+                    <img class="rounded-xl h-36 w-full object-cover" src="{{ asset('img/home/simple.jpg') }}" alt="">
                 </figure>
 
                 <header class="mt-2">
-                    <h1 class="text-center text-xl text-gray-700">mom</h1>
+                    <h1 class="text-center text-xl text-gray-700">Simplicidad</h1>
                 </header>
 
                 <p class="text-sm text-gray-500">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quia aliquid
@@ -66,11 +69,11 @@
 
             <article>
                 <figure>
-                    <img class="rounded-xl h-36 w-full object-cover" src="{{ asset('img/home/digital.jpg') }}" alt="">
+                    <img class="rounded-xl h-36 w-full object-cover" src="{{ asset('img/home/seguro.png') }}" alt="">
                 </figure>
 
                 <header class="mt-2">
-                    <h1 class="text-center text-xl text-gray-700">venado</h1>
+                    <h1 class="text-center text-xl text-gray-700">Seguridad</h1>
                 </header>
 
                 <p class="text-sm text-gray-500">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quia aliquid
@@ -79,9 +82,10 @@
         </div>
     </section>
 
+    {{-- catalog section --}}
     <section class="mt-24 bg-gray-700 py-12">
-        <h1 class="text-center text-white text-3xl">No se que hacer con mi laif</h1>
-        <p class="text-center text-white">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+        <h1 class="text-center text-white text-3xl">¿No sabes por donde empezar?</h1>
+        <p class="text-center text-white">Dale una revisada a nuestras categorias de productos y tipos de servicio</p>
 
         <div class="flex justify-center mt-4">
             <a href="#"
@@ -91,6 +95,7 @@
         </div>
     </section>
 
+    {{-- cards section --}}
     <section class="mt-24">
         <div>
             <h1 class="text-center text-gray-500 my-auto mx-auto">Ultimos posts</h1>
@@ -114,5 +119,4 @@
             @endforeach
         </div>
     </section>
-
 </x-app-layout>
