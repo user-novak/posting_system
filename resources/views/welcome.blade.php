c19b3e543da93e038422c1730632eef1b8d5ca41
Cristhian-elvis <cristhianvv2@gmail.com>
2021-09-15 23:31:59 -0500

    Finalizando la vista home

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -100,18 +100,27 @@
         <div>
             <h1 class="text-center text-gray-500 my-auto mx-auto">Ultimos posts</h1>
         </div>
-        <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 grid grid-cols-4 gap-x-6 gap-y-8">
+        <div
+            class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-x-6 gap-y-8">
             @foreach ($posts as $post)
                 @foreach ($imgs as $img)
                     @if ($img->id == $post->id)
-                        <article class="bg-white shadow-lg rounded">
-                            <img src="{{ $img->url }}" alt=""><br>
+                        <article class="bg-white shadow-lg rounded overflow-hidden">
+                            <img class="h-35 w-full object-cover" src="{{ $img->url }}" alt=""><br>
                             <div class="px-6 py-4">
-                                <h1>{{ $post->title }}</h1>
+                                <h1>{{ Str::limit($post->title, 20) }}</h1>
                                 <hr style="height:2px;border-width:0;color:gray;background-color:gray">
-                                <p>{{$post->product->description}}</p>
-                                <h3>tag</h3>
-                                <p>{{$post->product->category->name}}</p>
+                                <p>{{ Str::limit($post->product->description, 40) }}</p>
+                                <div class="flex">
+                                    <p class="text-sm text-gray-500 ml-auto">
+                                        <i class="fas fa-tags"></i>
+                                        {{ $post->product->category->name }}
+                                    </p>
+                                </div>
+                                <a href="#"
+                                    class="block text-center w-full mt-4 py-2 px-4 bg-green-500 text-white font-semibold rounded-lg shadow-md hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-400 focus:ring-opacity-75">
+                                    Más información
+                                </a>
                             </div>
                         </article>
                     @endif
