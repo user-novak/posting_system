666a8b8195ef5a357d6c306c2d898b1946fa35ac
nilsonlemoos <nilson.lemos@unas.edu.pe>
2021-09-16 21:49:56 -0500

    borrado de algunos comentarios en archivos de la carpeta database

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -22,7 +22,7 @@ class UserFactory extends Factory
      *
      * @return array
      */
-    public function definition()
+    public function definition()     
     {
         return [
             'name' => $this->faker->name(),
